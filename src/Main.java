9be5725dad9d4f60bf731a0444176fc0a488e729
Rimlin <Rimlin@gmail.com>
2019-01-06 17:12:32 +0200

    exempl32

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,45 @@
 import java.util.Scanner;
 
-//). Вычислить сумму и число положительных элементов матрицы A[N, N], находящихся над главной диагональю.
+//Дана матрица А размером п х т. Определить k – количество особых
+// элементов массива А, считая его элемент особым, если он больше
+// суммы остальных элементов его столбца.
 public class Main {
 
     public static void main(String[] args) {
         int N = 0;
-        int m = 0;
-        System.out.println("Введите размер матричы NxN");
+        //int m = 0;
+        System.out.println("Введите размер матричы nxt");
         Scanner sc = new Scanner(System.in);
         int n = 0;
-
         if (sc.hasNextInt()) {
             N = sc.nextInt();
+            int[] m = new int[N];
             int[][] mass = new int[N][N];
+            // int m[N]=0;
             for (int i = 0; i < mass.length; i++) {
                 System.out.println();
                 for (int j = 0; j < mass.length; j++) {
-                    mass[i][j] = (int) (Math.random() * 10);
-                    if (mass[i][j] % 2 == 0) {
-                    }
+                    mass[i][j] = (int) (Math.random() * 100);
                     System.out.print(" " + mass[i][j]);
                 }
             }
             System.out.println();
             for (int i = 0; i < mass.length; i++) {
-                if (i < N - 1 && mass[i][i + 1] != 0 && mass[i][i + 1] % 2 == 0) {
-                    m = m + mass[i][i + 1];
-                    n++;
+                for (int j = 0; j < mass.length; j++) {
+                    m[i] = m[i] + mass[j][i];
+                }
+            }
+            System.out.println();
+            for (int i = 0; i < mass.length; i++) {
+                for (int j = 0; j < mass.length; j++) {
+
+                    if (mass[j][i] > (m[i] - mass[j][i])) {
+                        n++;
+                    }
                 }
             }
         }
-        System.out.print("m= " + m + " n= " + n);
+        System.out.print("Особых элементов массива " + n);
     }
 }
 
